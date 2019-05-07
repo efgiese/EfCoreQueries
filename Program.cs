349e605b67b490aafd4357d75e021a232a0383f2
Edgar Giese <edgar@egiese.de>
2019-05-07 21:17:45 +0200

    Setup Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queries.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
