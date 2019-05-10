6424e05258709f2145bad9b59f98678b66741428
Edgar Giese <edgar@egiese.de>
2019-05-10 19:13:57 +0200

    CourseCover Model

---
+++ Model/CourseCover.cs
@@ -0,0 +1,9 @@
+﻿namespace Queries.Core.Model
+{
+  public class CourseCover
+  {
+    public int CourseCoverId { get; set; }
+    public int CoverOfCourseId { get; set; }
+    public Course Course { get; set; }
+  }
+}
