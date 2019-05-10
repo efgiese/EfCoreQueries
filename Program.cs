bbd0339c71cc9f301485bf20bdb6943dbfdcc300
Edgar Giese <edgar@egiese.de>
2019-05-10 19:10:54 +0200

    Format

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace Queries.Core
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
     }
+  }
 }
